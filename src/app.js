24fc9f1a8c538fee15f62637e11fb9a9d09093f0
lrmendozacicese <57477221+lrmendozacicese@users.noreply.github.com>
2019-11-21 21:37:21 -0800

    lrmendoza
    new changes

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ const express =require('express');
 const cors =require('cors');
 const app = express();
 
-
+//settings
+app.set('port',process.env.PORT || 4000)
 
 
 //middlewares
--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const URI = process.env.MONGODB_URI ? process.env.MONGODB_URI :'mongodb://localhost/beerdatabase';
+const URI = process.env.MONGODB_URI ? process.env.MONGODB_URI :'mongodb+srv://lrmendoza:mendoza260695@cluster0-8xh0p.mongodb.net/test?retryWrites=true&w=majority';
 
 mongoose.connect(URI, {
     useNewUrlParser:true,
