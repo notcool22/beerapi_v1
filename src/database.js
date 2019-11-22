a860764f655623724935e0c4096f842261572754
lrmendozacicese <57477221+lrmendozacicese@users.noreply.github.com>
2019-11-21 22:15:52 -0800

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const URI = process.env.MONGODB_URI ? process.env.MONGODB_URI :'mongodb+srv://lrmendoza:mendoza260695@cluster0-8xh0p.mongodb.net/test?retryWrites=true&w=majority';
+const URI = process.env.MONGODB_URI ? process.env.MONGODB_URI :'mongodb+srv://lrmendoza@cicese.edu.mx:mendoza260695@cluster0-8xh0p.mongodb.net/test?retryWrites=true&w=majority';
 
 mongoose.connect(URI, {
     useNewUrlParser:true,
