95796d4888ce0f60c1de540d9bb7385461f3cee1
lrmendozacicese <57477221+lrmendozacicese@users.noreply.github.com>
2019-11-26 14:01:13 -0800

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,13 +1,24 @@
 const mongoose = require('mongoose');
 
-const URI = 'mongodb+srv://lrmendoza:luisroberto@cluster0-8xh0p.mongodb.net/test?retryWrites=true&w=majority';
-
+// const URI = 'mongodb+srv://lrmendoza:luisroberto@cluster0-8xh0p.mongodb.net/test?retryWrites=true&w=majority';
+/*
 mongoose.connect(URI, {
     useNewUrlParser:true,
     useCreateIndex:true
-});
-const connection = mongoose.connection;
+});*/
 
+// const connection = mongoose.connection;
+/*
 connection.once('open', ()=> {
     console.log('DB is connected')
+});
+*/
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://lrmendoza:luisroberto@cluster0-8xh0p.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true,useCreateIndex:true});
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
 });
