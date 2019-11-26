73f095d46542b5b71c185db8587a66e9b4eaf9a6
lrmendozacicese <57477221+lrmendozacicese@users.noreply.github.com>
2019-11-26 13:47:20 -0800

    lr men
    4 attemp

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const URI = 'mongodb+srv://lrmendoza@cicese.edu.mx:<luisroberto>@cluster0-8xh0p.mongodb.net/test?retryWrites=true&w=majority';
+const URI = 'mongodb+srv://lrmendoza:luisroberto@cluster0-8xh0p.mongodb.net/test?retryWrites=true&w=majority';
 
 mongoose.connect(URI, {
     useNewUrlParser:true,
