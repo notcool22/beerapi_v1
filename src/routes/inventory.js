60c8e89fd780901ee186e9f45bac9cc43d13624d
notcool22 <45542351+notcool22@users.noreply.github.com>
2019-11-27 20:19:30 -0800

    Update inventory.js

--- src/routes/inventory.js
+++ src/routes/inventory.js
@@ -3,7 +3,8 @@ const router= Router();
 const {getfullInventory, createInventory, getInventory, updateInventory, deleteInventory,findBeer,findBrewery} = require('../controllers/inventory.controllers')
 router.route('/')
     .post (createInventory)
+    .delete(deleteInventory)
+router.route('/:id')
     .get( findBeer)
     .delete(deleteInventory)
-    
 module.exports = router;
